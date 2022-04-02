47f580fb6963a031534078dd02c1d427e28443f9
Stroil <edinstroil@hotmail.com>
2022-04-02 10:07:34 +0200

    Broj slova

---
+++ ex09/src/index.js
@@ -0,0 +1,14 @@
+function main(){
+    // only change code below this line
+    var lastName = "Doe";
+    var lastNameLength ;
+    lastNameLength = lastName.length ;
+    // only change code above this line
+    
+return lastNameLength;
+        
+    
+}
+
+console.log(main());
+module.exports = main;
