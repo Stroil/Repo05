e26819db6f71238305165b93be300de9c69ec40c
Stroil <edinstroil@hotmail.com>
2022-04-02 08:42:01 +0200

    Literal Quote Escaped

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+    // only change code below this line
+    var myFirstName = "Edin";
+    var myLastName = "Stroil";
+    var myHomeTown = "Sarajevo";
+    // only change code above this line
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
